8186115cd2a562bf9c9908d6ef24db9f6f1e60cf
cryi <cryi@tutanota.com>
2022-11-21 21:16:05 +0100

    use main branch, tools/inject

---
+++ tools/inject.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# tezpay command reference
+
+rm -rf content/injected/tezpay/cmd/*
+mkdir -p content/injected/tezpay/cmd/
+export CWD=$PWD
+cd projects/tezpay/docs/cmd || exit 1
+for f in *.md; do
+	echo "Processing $f file...";
+	(echo "---";
+	echo "title: $(printf "%s" "$f" | cut -d. -f1)";
+	echo "weight: 3";
+	echo "type: docs";
+	echo "---") | \
+	cat - "$f" > "$CWD/content/injected/tezpay/cmd/$f"
+done
+cd "$CWD" || exit 1
+
+# tezpay configuration samples
+rm -rf content/injected/tezpay/configuration/*
+mkdir -p content/injected/tezpay/configuration/
+cd projects/tezpay/docs/configuration || exit 1
+for f in config.*.hjson; do
+	echo "Processing $f file...";
+	(echo "---";
+	echo "title: $(printf $f | cut -d. -f2)";
+	echo "weight: 3";
+	echo "type: docs";
+	echo "summary: tezpay $(printf $f | cut -d. -f2) configuration";
+	echo "---";
+	echo '```yaml') | \
+	cat - "$f" > "$CWD/content/injected/tezpay/configuration/$(printf $f | cut -d. -f2).md"
+	(echo ''; echo '```') >> "$CWD/content/injected/tezpay/configuration/$(printf $f | cut -d. -f2).md"
+done
+cd "$CWD" || exit 1
