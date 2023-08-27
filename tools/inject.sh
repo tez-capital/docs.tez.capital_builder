6a2c2f1d10037da1d3a59992a6d0ccb3f959ca7f
cryi <cryi@tutanota.com>
2023-08-27 23:15:45 +0200

    tezbake command reference

--- tools/inject.sh
+++ tools/inject.sh
@@ -47,3 +47,20 @@ for f in *.md; do
 	echo "---") | \
 	cat - "$f" > "$CWD/content/injected/tezpay/extensions/$f"
 done
+
+# tezbake command reference
+
+rm -rf content/injected/tezbake/cmd/*
+mkdir -p content/injected/tezbake/cmd/
+export CWD=$PWD
+cd projects/tezbake/docs/cmd || exit 1
+for f in *.md; do
+	echo "Processing $f file...";
+	(echo "---";
+	echo "title: $(printf "%s" "$f" | cut -d. -f1)";
+	echo "weight: 3";
+	echo "type: docs";
+	echo "---") | \
+	cat - "$f" > "$CWD/content/injected/tezbake/cmd/$f"
+done
+cd "$CWD" || exit 1
